adaffd4e284175595a0385573c7a0d99518d1911
Nachiket Gomkale <nachiketgomkale15@gmail.com>
2023-01-18 15:38:51 +0530

    update app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import FooterSection from "./components/Footer/FooterSection";
-import HeroSection from "./components/Hero/HeroSection";
-import HowItworks from "./components/How-it-works/HowItworks";
-import LoginForm from "./components/Login/LoginForm";
-import Navbar from "./components/Navbar/Navbar";
-import SecondaryNavbar from "./components/Navbar/SecondaryNavbar";
-import SignupForm from "./components/Signup/SignupForm";
-
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <SecondaryNavbar />
-      {/* <HeroSection />
-      <FooterSection />
-      <HowItworks />  */}
-      {/* <LoginForm /> */}
-      {/* <SignupForm /> */}
-=======
 import DestinationCard from "./components/Destination/DestinationCard.jsx";
 import FAQSection from "./components/FAQ/Index.jsx";
 import CategoriesCards from "../src/components/DestinationCategory/CategoriesCards.jsx";
@@ -29,7 +9,6 @@ function App() {
       <DestinationCard />
       <FAQSection />
       <CategoriesCards />
->>>>>>> dev-frontend
     </div>
   );
 }
