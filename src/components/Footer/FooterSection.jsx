53a43579c6328bb9384abb15307764af842c8e57
Nachiket Gomkale <nachiketgomkale15@gmail.com>
2023-01-18 15:41:20 +0530

    Footersection conflicts solved

--- src/components/Footer/FooterSection.jsx
+++ src/components/Footer/FooterSection.jsx
@@ -1,16 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-<<<<<<< HEAD
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  height: 40vh;
-  padding: 3rem;
-  background-color: #e4d9d9;
-=======
 import GoogleIcon from "../../assets/images/SocialButtonGoogle.png";
 import TwitterIcon from "../../assets/images/SocialButtonTwitter.png";
 import FacebookIcon from "../../assets/images/SocialButtonFacebook.png";
@@ -41,7 +30,6 @@ const Image = styled.img`
   width: 250px;
   height: 250px;
   mix-blend-mode: multiply;
->>>>>>> dev-frontend
 `;
 
 const Description = styled.div`
@@ -67,58 +55,15 @@ const FeaturesContainer = styled.div`
   }
 `;
 
-<<<<<<< HEAD
-=======
 const listItem = {
   fontWeight: "bold",
 };
 
->>>>>>> dev-frontend
 const Features = styled.div``;
 
 function FooterSection() {
   return (
     <Container>
-<<<<<<< HEAD
-      <Description>
-        <p>Who we are?</p>
-        <p>
-          Obsessed with the idea of empowering the travelers with best vacation
-          deals, We are an online marketplace that connects a traveler to
-          multiple local travel agents. With the help of these 650+ local travel
-          experts TravelTriangle has been able to cater to the needs of over 10
-          Lac travelers on domestic India tours.
-        </p>
-      </Description>
-      <FeaturesContainer>
-        <Features>
-          <ul>
-            <li>Mobile app</li>
-            <li>Features</li>
-            <li>Live Share</li>
-            <li>video record</li>
-          </ul>
-        </Features>
-
-        <Features>
-          <ul>
-            <li>Community</li>
-            <li>lorem ipsum</li>
-            <li>Make my trip</li>
-            <li>Live events</li>
-          </ul>
-        </Features>
-
-        <Features>
-          <ul>
-            <li>About us</li>
-            <li>Contact us</li>
-            <li>Gallaery</li>
-            <li>lorem ipsum</li>
-          </ul>
-        </Features>
-      </FeaturesContainer>
-=======
       <Wrapper>
         <Description>
           <p>Who we are?</p>
@@ -162,7 +107,6 @@ function FooterSection() {
       <FooterImg>
         <Image src={FooterImage} />
       </FooterImg>
->>>>>>> dev-frontend
     </Container>
   );
 }
