feb4a65ae613002658f5e52f7beefcaad1333c93
Nachiket Gomkale <nachiketgomkale15@gmail.com>
2023-01-18 15:47:25 +0530

    Searchbar conflict resolve

--- src/components/Hero/Searchbar.jsx
+++ src/components/Hero/Searchbar.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-<<<<<<< HEAD
-
-import React from "react";
-
-const Container = styled.div``;
-=======
 import HeroBackground from "../../assets/images/Hero-section-bg.webp";
 
 const Container = styled.div`
@@ -25,19 +19,14 @@ const Container = styled.div`
     margin-bottom: 25px;
   }
 `;
->>>>>>> dev-frontend
 
 function Searchbar() {
   return (
     <Container>
-<<<<<<< HEAD
-      <h3> Customizing And Book Amazing Holiday Packages</h3>
-=======
       <h3>
         Customizing And Book <br />
         Amazing Holiday Packages
       </h3>
->>>>>>> dev-frontend
       <p>650+ Travel Agents serving 65+ Destinations worldwide</p>
     </Container>
   );
