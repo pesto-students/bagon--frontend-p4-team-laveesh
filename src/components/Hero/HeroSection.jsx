e6324cbea22800a73f35496b04fd283a0f59356d
Nachiket Gomkale <nachiketgomkale15@gmail.com>
2023-01-18 15:44:04 +0530

    HeroSection conflicts resolve

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import HeroSectionImage from "../../assets/images/Hero-section-1.webp";
-<<<<<<< HEAD
-
-const Container = styled.div`
-  width: 100%;
-  height: 50vh;
-=======
 import HeroSectionImage2 from "../../assets/images/Hero-section-2.webp";
 import Searchbar from "./Searchbar";
 
 const Container = styled.div`
   width: 100%;
   height: 60vh;
->>>>>>> dev-frontend
   display: flex;
   align-items: center;
   justify-content: center;
@@ -21,23 +14,17 @@ const Container = styled.div`
 
 const ImgContainer = styled.div`
   height: 100%;
-<<<<<<< HEAD
-=======
   width: 100%;
   flex: 1;
 `;
 
 const SearchbarContainer = styled.div`
->>>>>>> dev-frontend
   flex: 1;
 `;
 
 const Image = styled.img`
   height: 100%;
-<<<<<<< HEAD
-=======
   width: 100%;
->>>>>>> dev-frontend
 `;
 
 function HeroSection() {
@@ -46,15 +33,12 @@ function HeroSection() {
       <ImgContainer>
         <Image src={HeroSectionImage} />
       </ImgContainer>
-<<<<<<< HEAD
-=======
       <SearchbarContainer>
         <Searchbar />
       </SearchbarContainer>
       <ImgContainer>
         <Image src={HeroSectionImage2} />
       </ImgContainer>
->>>>>>> dev-frontend
     </Container>
   );
 }
